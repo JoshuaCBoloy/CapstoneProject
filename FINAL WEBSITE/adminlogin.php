ae21210d3e11384d544d494dc5d75132d4df31aa
JamesDDando <jdd6763@students.uc-bcf.edu.ph>
2024-02-13 19:50:27 +0800

    New dashboard layout

--- FINAL WEBSITE/adminlogin.php
+++ FINAL WEBSITE/adminlogin.php
@@ -1,37 +1,81 @@
+<?php
+session_start();
+if (isset($_SESSION["user"])) {
+   header("Location: index.html");
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-	<meta charset="UTF-8">
-	<link rel="stylesheet" href=
-"https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<link rel="stylesheet" href="css/login.css">
-	<title>Login Page</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+	<link rel="stylesheet" type="text/css" href="css/new-style.css">
+	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
+	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
 </head>
-
 <body>
-	<form action="adminvalidate.php" method="post">
-		<div class="login-box">
-			<h1>Tour Guide Login</h1>
+    <img  class="wave" src="image/wave.png">
+    <div class="container">
+        <?php
+        if (isset($_POST["login"])) {
+           $email = $_POST["email"];
+           $password = $_POST["password"];
+            require_once "database.php";
+            $sql = "SELECT * FROM users WHERE email = '$email'";
+            $result = mysqli_query($conn, $sql);
+            $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
+            if ($user) {
+                if (password_verify($password, $user["password"])) {
+                    session_start();
+                    $_SESSION["user"] = "yes";
+                    header("Location: index.html");
+                    die();
+                }else{
+                    echo "<div class='alert alert-danger'>Password does not match</div>";
+                }
+            }else{
+                echo "<div class='alert alert-danger'>Email does not match</div>";
+            }
+        }
+        ?>
+        
+        <div class="img">
+            <img src="image/ELT.png">
+        </div>
+        <div class="login-content">
+            <form action="adminvalidate.php" method="post">
+                <img src="image/avatar.svg">
+                <h2  class="title">Log In</h2>
+                <div class="input-div one">
+                    <div class="i">
+                        <i class="fas fa-envelope"></i>
+                    </div>
+                    <div class="div">
+                        <h5>Username</h5>
+                        <input type="text" class="input" name="username">
+                    </div>
+                </div>
+                
+                <div class="input-div pass">
+                    <div class="i">
+                        <i class="fas fa-lock"></i>
+                    </div>
+                    <div class="div">
+                        <h5>Password</h5>
+                        <input type="password" class="input" name="password">
+                    </div>
+                </div>
 
-			<div class="textbox">
-				<i class="fa fa-user" aria-hidden="true"></i>
-				<input type="text" placeholder="Username"
-						name="username" value="">
-			</div>
+                <div class="form-btn">
+                    <input type="submit" class="btn btn-primary" value="Login" name="login">
+                </div>
+            </form>
+        </div>
 
-			<div class="textbox">
-				<i class="fa fa-lock" aria-hidden="true"></i>
-				<input type="password" placeholder="Password"
-						name="password" value="">
-			</div>
+    <script type="text/javascript" src="js/main.js"></script>
 
-			<input class="button" type="submit"
-					name="login" value="Sign In">
-		</div>
-	</form>
 </body>
-
 </html>
