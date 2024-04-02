8148cce3e44b3c4dbfb17307e6a1fde1b6c829a5
Jashqtieee <instajoshua04@gmail.com>
2024-04-03 01:59:34 +0800

    added email verification and password reset using PHP Mailer

--- FINAL WEBSITE/Logged-in-Page/LIP-login.php
+++ FINAL WEBSITE/Logged-in-Page/LIP-login.php
@@ -1,8 +1,37 @@
 <?php
-session_start();
-if (isset($_SESSION["user"])) {
-   header("Location: LIP-index.html");
+
+$is_invalid = false;
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    
+    $mysqli = require __DIR__ . "/LIP-database.php";
+    
+    $sql = sprintf("SELECT * FROM user
+                    WHERE email = '%s'",
+                   $mysqli->real_escape_string($_POST["email"]));
+    
+    $result = $mysqli->query($sql);
+    
+    $user = $result->fetch_assoc();
+    
+    if ($user && $user["account_activation_hash"] == null) {
+        
+        if (password_verify($_POST["password"], $user["password_hash"])) {
+            
+            session_start();
+            
+            session_regenerate_id();
+            
+            $_SESSION["user_id"] = $user["id"];
+            
+            header("Location: LIP-index.html");
+            exit;
+        }
+    }
+    
+    $is_invalid = true;
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -17,30 +46,9 @@ if (isset($_SESSION["user"])) {
 	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
 </head>
 <body>
+
     <img  class="wave" src="image/wave.png">
     <div class="container">
-        <?php
-        if (isset($_POST["login"])) {
-           $email = $_POST["email"];
-           $password = $_POST["password"];
-            require_once "LIP-database.php";
-            $sql = "SELECT * FROM users WHERE email = '$email'";
-            $result = mysqli_query($conn, $sql);
-            $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
-            if ($user) {
-                if (password_verify($password, $user["password"])) {
-                    session_start();
-                    $_SESSION["user"] = "yes";
-                    header("Location: LIP-index.html");
-                    die();
-                }else{
-                    echo "<div class='alert alert-danger'>Password does not match</div>";
-                }
-            }else{
-                echo "<div class='alert alert-danger'>Email does not match</div>";
-            }
-        }
-        ?>
         
         <div class="img">
             <img src="image/ELT.png">
@@ -55,7 +63,7 @@ if (isset($_SESSION["user"])) {
                     </div>
                     <div class="div">
                         <h5>Email Address</h5>
-                        <input type="email" class="input" name="email">
+                        <input type="email" class="input" name="email" value="<?= htmlspecialchars($_POST["email"] ?? "") ?>">
                     </div>
                 </div>
                 
@@ -69,8 +77,10 @@ if (isset($_SESSION["user"])) {
                     </div>
                 </div>
                 
-            	<a href="LIP-registration.php">Don't have an account yet? Register Now</a>
-                <a href="http://localhost/CapstoneProject/FINAL%20WEBSITE/index.html">Go to website</a>
+            	<a href="registration.php">Don't have an account yet? Register Now</a>
+                <a href="http://localhost/CapstoneProject/FINAL%20WEBSITE/LIP-index.html">Go to website</a>
+
+                <a href="LIP-forgot-password.php">Forgot Password?</a>
 
                 <div class="form-btn">
                     <input type="submit" class="btn btn-primary" value="Login" name="login">
