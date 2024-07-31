eee8a0933122ae1e63c86075932669e5e9bccdc0
Jashqtieee <instajoshua04@gmail.com>
2024-07-31 21:33:03 +0800

    changes in the registration and login page

--- Final Website/Logged-in-Page/LIP-login.php
+++ Final Website/Logged-in-Page/LIP-login.php
@@ -46,9 +46,276 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <script src="https://kit.fontawesome.com/a81368914c.js"></script>
     <link rel="shortcut icon" type="image/x-icon" href="image/ELT.png" />
     <style>
-        .error {
-            color: red;
-        }
+        * {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Poppins", sans-serif;
+  overflow: hidden;
+  background-color: #f8f9fa;
+}
+
+.wave {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  height: 100%;
+  z-index: -1;
+}
+
+.container {
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 7rem;
+  padding: 0 2rem;
+  justify-content: center;
+  align-items: center;
+}
+
+.img {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+
+.login-content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  padding: 40px;
+}
+
+.img img {
+  width: 500px;
+}
+
+form {
+  width: 100%;
+}
+
+.login-content img {
+  height: 100px;
+}
+
+.login-content h2 {
+  margin: 15px 0;
+  color: #333;
+  text-transform: uppercase;
+  font-size: 2.9rem;
+}
+
+.row {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+
+.col {
+  flex: 0 0 48%;
+}
+
+.col-12 {
+  flex: 0 0 100%;
+}
+
+.input-div {
+  position: relative;
+  display: grid;
+  grid-template-columns: 7% 93%;
+  margin: 15px 0;
+  padding: 5px 0;
+  border-bottom: 2px solid #d9d9d9;
+  width: 100%;
+}
+
+.i {
+  color: #d9d9d9;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.i i {
+  transition: 0.3s;
+}
+
+.input-div > div {
+  position: relative;
+  height: 45px;
+  margin-bottom: 10px;
+}
+
+.input-div > div > h5 {
+  position: absolute;
+  left: 10px;
+  top: 50%;
+  transform: translateY(-50%);
+  color: #999;
+  font-size: 18px;
+  transition: 0.3s;
+}
+
+.input-div > div > input:not(:placeholder-shown) + h5 {
+  top: -5px;
+  font-size: 15px;
+}
+
+.input-div:before,
+.input-div:after {
+  content: "";
+  position: absolute;
+  bottom: -2px;
+  width: 0%;
+  height: 2px;
+  background-color: #38d39f;
+  transition: 0.4s;
+}
+
+.input-div:before {
+  right: 50%;
+}
+
+.input-div:after {
+  left: 50%;
+}
+
+.input-div.focus:before,
+.input-div.focus:after {
+  width: 50%;
+}
+
+.input-div.focus > div > h5 {
+  top: -5px;
+  font-size: 15px;
+}
+
+.input-div.focus > .i > i {
+  color: #38d39f;
+}
+
+.input-div > div > input {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  border: none;
+  outline: none;
+  background: none;
+  padding: 0.5rem 0.7rem;
+  font-size: 1.2rem;
+  color: #555;
+  font-family: "Poppins", sans-serif;
+}
+
+a {
+  display: block;
+  text-align: right;
+  text-decoration: none;
+  color: #999;
+  font-size: 0.9rem;
+  transition: 0.3s;
+}
+
+a:hover {
+  color: #38d39f;
+}
+
+.btn {
+  display: block;
+  width: 100%;
+  height: 50px;
+  border-radius: 25px;
+  outline: none;
+  border: none;
+  background-image: linear-gradient(to right, #32be8f, #38d39f, #32be8f);
+  background-size: 200%;
+  font-size: 1.2rem;
+  color: #fff;
+  font-family: "Poppins", sans-serif;
+  text-transform: uppercase;
+  margin: 1rem 0;
+  cursor: pointer;
+  transition: 0.5s;
+}
+.btn:hover {
+  background-position: right;
+}
+
+.error {
+  color: red;
+  font-size: 12px;
+  margin-top: 1px;
+  margin-bottom: 40px;
+}
+
+.signup-text {
+  text-align: center;
+  margin-top: 17rem;
+}
+
+.signup-text a {
+  text-decoration: none;
+  color: #999;
+  font-size: 30px;
+  font-weight: bold;
+  transition: 0.3s;
+  text-align: center;
+}
+
+.signup-text a:hover {
+  color: #38d39f;
+}
+
+@media screen and (max-width: 1050px) {
+  .container {
+      grid-gap: 5rem;
+  }
+}
+
+@media screen and (max-width: 1000px) {
+  form {
+      width: 290px;
+  }
+
+  .login-content h2 {
+      font-size: 2.4rem;
+      margin: 8px 0;
+  }
+
+  .img img {
+      width: 400px;
+  }
+}
+
+@media screen and (max-width: 900px) {
+  .container {
+      grid-template-columns: 1fr;
+  }
+
+  .img {
+      display: none;
+  }
+
+  .wave {
+      display: none;
+  }
+
+  .login-content {
+      justify-content: center;
+  }
+
+  .col {
+      flex: 0 0 100%;
+  }
+}
+
     </style>
 </head>
 <body>
